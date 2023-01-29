25b9f4fdee18b8ab9f3de13760f73f2a0e7a8553
Kevin-AC <kevin.argumedo@gmail.com>
2023-01-28 21:32:59 -0500

    p2

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ $form.addEventListener('submit',sendEmail)
 function sendEmail(event){
   event.preventDefault()
   const form=new FormData(this)
-  sendmail.setAttribute('href',`mailto:kevin.argumedo@gmail.com?subject=${form.get(email)}&body=${form.get(mensaje)}`)
+  sendmail.setAttribute('href',`mailto:kevin.argumedo@gmail.com?subject=${form.get('email')}&body=${form.get('mensaje')}`)
   sendmail.click()
 }

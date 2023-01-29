49b8a24d8cfaa9e28468c93c95728fb61c07d1f1
Kevin-AC <kevin.argumedo@gmail.com>
2023-01-28 19:21:19 -0500

    diseño listo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,9 @@ module.exports = {
       colors:{
         'secundario':'#F7F7FF',
         'azulOscuro':'#24253c',
-        'gris':'#2F3233',
         'grisClaro':'#7B817F',
         'principal':'#070600',
-        'black':'#040410',
         'Alabaster':'#E8EBE4',
-        // 'xanadu':'#FFFCF9',
-        // 'platinum':'#E7E5DF',
-        // 'xiketic':'#0F1020'
       },
       fontFamily:{
         'Roboto':'Roboto', 
